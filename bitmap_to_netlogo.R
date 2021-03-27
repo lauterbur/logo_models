23a1533bad24e04b14cbc624d3c2fe2061c78d41
M. Elise Lauterbur <lauterbur@users.noreply.github.com>
2021-03-26 23:34:10 -0700

    map color correction

--- bitmap_to_netlogo.R
+++ bitmap_to_netlogo.R
@@ -11,12 +11,12 @@ plot(1, type = "n", axes = FALSE, xlab = "", ylab = "")
 usr <- par("usr")
 rasterImage(m, usr[1], usr[3], usr[2], usr[4])
 
-dict<-setNames(c(seq(2,112,10)),unique(m))
+dict<-setNames(c(7,9.9,115,66,15,25,87,1,45,62,33,103),unique(m))
 dict
-m_vec<-matrix(unname(dict[as.vector(m)]), nrow=nrow(m),byrow=TRUE)
-m_vec
+m_matrix<-matrix(unname(dict[as.vector(m)]), nrow=nrow(m),byrow=TRUE)
+m_matrix
 
-coords<-data.frame(x=as.vector(col(map_matrix)),y=as.vector(row(map_matrix)),color=as.vector(map_matrix))
+coords<-data.frame(x=as.vector(col(m_matrix)),y=rev(as.vector(row(m_matrix))),color=as.vector(m_matrix))
 coords<-t(coords)
 coords[3,]
 
